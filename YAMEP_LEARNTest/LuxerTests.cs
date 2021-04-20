35927812f5e84630e0e016285fe398d259b02eea
Maksym Kurkudiuk <mkurkudiuk@deloitte.com>
2021-04-20 11:58:58 +0300

    Scanning and Lexing Tokens

---
+++ YAMEP_LEARNTest/LuxerTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using YAMEP_LEARN;
+
+namespace YAMEP_LEARNTest {
+    [TestClass]
+    public class UnitTest1 {
+
+        [TestMethod()]
+        public void ReadNextTest() {
+            var exprission = "1 + 2";
+
+            (Token.TokenType, int, string)[] expectedResults = new (Token.TokenType, int, string)[] {
+                (Token.TokenType.NUMBER, 0, "1"),
+                (Token.TokenType.Addition, 2, "+"),
+                (Token.TokenType.NUMBER, 4, "2"),
+            };
+
+            var lexer = new Lexer(new SourceScanner(exprission));
+
+            foreach (var (t, p, v) in expectedResults) {
+                var token = lexer.ReadNext();
+                Assert.AreEqual(t, token.Type);
+                Assert.AreEqual(p, token.Position);
+                Assert.AreEqual(v, token.Value);
+            }
+
+            Assert.AreEqual(Token.TokenType.EOE, lexer.ReadNext().Type);
+        }
+    }
+}
---
+++ YAMEP_LEARNTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("YAMEP_LEARNTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("YAMEP_LEARNTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4b39551c-7bab-458f-ba73-bf95889ee578")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
